be5b88b5ce9d02adefe210b0ef95d0c4b2738df0
nitya <nityabhaskar21@gmail.com>
2021-06-21 19:43:02 +0530

    First Commit

---
+++ src/main/java/com/projectN/app/ProjectNApplication.java
@@ -0,0 +1,13 @@
+package com.projectN.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectN/app/ProjectNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectN.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
