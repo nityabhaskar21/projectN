9fd9a2e481406774920d39c83ca9b97dd580f36a
nitya <nityabhaskar21@gmail.com>
2021-07-25 11:47:58 +0530

    added http url in cors

--- src/main/java/com/projectN/app/ProjectNApplication.java
+++ src/main/java/com/projectN/app/ProjectNApplication.java
@@ -20,7 +20,11 @@ public class ProjectNApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://client-projectn.herokuapp.com"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList(
+				"http://localhost:4200", 
+				"https://localhost:4200",
+				"https://client-projectn.herokuapp.com", 
+				"http://client-projectn.herokuapp.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
